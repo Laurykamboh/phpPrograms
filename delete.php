4a8583df3e712f7ebdb846f74d03ede02d52c6fd
Laurykamboh <68334354+Laurykamboh@users.noreply.github.com>
2021-03-30 15:17:17 +0100

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,34 @@
+<?php
+
+
+if(isset($_POST["id"]) && !empty(trim($_POST["id"]))){
+	require_once "config.php";
+
+	$sql = "DELETE FROM employees WHERE id = ?";
+
+	if($stmt = mysqli_prepare($link, $sql)){
+		mysqli_stmt_bind_param($stmt, "i", $param_id);
+		$param_id = trim($_POST["id"]);
+
+		if(mysqli_stmt_execute($stmt)){
+			header("location: index.php");
+			exit();
+		}
+		else
+		{
+			echo "oops! Something went wrong. Please try again later.";
+		}
+		}
+		mysli_stmt_close($stmt);
+		mysqli_close($link);
+	}
+	else{
+		if(empty(trim($_GET["id"])))
+		{
+			header("location: error.php");
+			exit();
+		}
+	}
+
+
+?>
